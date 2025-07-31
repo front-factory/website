adcba6bb265bc1ce05142dc089e8889432facef6
Boris Damevin <borisdamevin@gmail.com>
2025-07-31 11:13:37 +0200

    feat: seo optimization

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import { useHead } from '@vueuse/head';
 
+useHead({
+    link: [
+        {
+            rel: 'preload',
+            as: 'image',
+            href: '/logo.svg',
+            type: 'image/svg+xml',
+            fetchpriority: 'high'
+        }
+    ]
+});
 </script>
 
 <template>
     <div :class="$style['wrapper']">
         <h1>
-            <img :class="$style['logo']" src="./assets/logo.svg" width="500" height="132" alt="Front Factory"
+            <img :class="$style['logo']" src="/logo.svg" width="500" height="132" alt="Front Factory"
                  fetchpriority="high"/>
         </h1>
     </div>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue';
+import { createHead } from '@vueuse/head';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+const head = createHead();
+
+app.use(head);
+app.mount('#app');
